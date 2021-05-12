72c08d6b675125903acebfb08df3cee893c74389
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-05-12 13:21:00 +0300

    pulse check

--- main.js
+++ main.js
@@ -1,31 +1,44 @@
+const { clear } = require("console");
 const express = require("express");
 
 const app = express();
 const port = 3000;
-
+app.use(express.json())
 const users = ["John", "Mark"];
+// const users = [];
 
+const logUsers = (req, res, next) => {
+    console.log(users);
+        next();
+    // if(users === []){
+    //     next("error")
+    // }else{
+    //     console.log(users);
+    //     next();
+    // }
+}
+const logMethod = (req, res, next) => {
+    console.log(req.method);
+    next();
+}
+app.use(logUsers);
+app.use("/users", logMethod)
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
+// app.use((err,req, res, next)=>{
+//     res.status(404);
+//     console.log("No users");
+//     // res.json("No users")
+// })
+// clear()
 
-const logUsers = (req, res, next) => {
-  console.log(users);
-  next();
-};
-app.use(logUsers);
 
-const logMethod = (req, res, next) => {
-    console.log(req.method);
-    next()
-}
-// logMethod.use((req, res, next) => {
-//   console.log(req.method);
-//   next();
-// });
-app.get("/users", logMethod, (req, res,next) => {
+app.post("/users/create", (req, res) =>{
+    const name = req.body.name;
+    users.push(name);
     res.json(users)
-});
+})
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+//   console.log(`app listening at http://localhost:${port}`);
 });
